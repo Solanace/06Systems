aa7eda3f55290f90caa21dc6ee901fe320869f1b
Penn Wu <penn.z.wu@gmail.com>
2017-10-13 10:59:10 -0400

    Started

---
+++ work06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    char c;
+    struct node *next;
+};
+
+void print_list(struct node *n) {
+    while (n.next) {
+        printf("%c\n", n.c);
+        n = n.next;
+    }
+}
+
+struct node *insert_front(struct node *n, char add_me) {
+}
